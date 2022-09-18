dd9874d8d28f36105c6245da555fb624f3d38d35
Orhan Zırhlı <98525340+Zirhli@users.noreply.github.com>
2022-09-18 20:33:07 +0300

    project

--- proje.py
+++ proje.py
@@ -1,11 +1,18 @@
+
+
 list_1=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 list_2=[]
-def flatten(x):
-  for i in x:
+
+def flatten(list_1):
+  for i in list_1:
     if isinstance(i,list):
       flatten(i)
-    else: 
+    else:
       list_2.append(i)
+
+flatten(list_1)
+
 print(list_2)
-      
+
+
     
